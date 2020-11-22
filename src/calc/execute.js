9b9fe2a3f0d211c04d1fe07f6c6b47575bc519e4
tasasei <29848455+tasasei@users.noreply.github.com>
2020-11-22 20:24:13 +0900

    create averageDamage()

--- src/calc/execute.js
+++ src/calc/execute.js
@@ -22,19 +22,28 @@ console.log(total/LOOP);
 // Param
 // Deck Num, CX Num, Flip Num, LOOP Num
 
-// 指定のダメージを与えたときの平均打点が知りたい
-let totalDamage = 0;
-for (let i=0; i<LOOP; i++){
-  let turnDamage = 0;
-  let deck = ws.createDeck(10,3);
-  // 与えたい打点[2点,2点,2点]
-  const DMG_ARRAY = [2,2,2];
-  DMG_ARRAY.forEach( dmg => {
-    const res = ws.cancelOrNot(deck,dmg);
-    deck = res.deck;
-    if(!res.cancel) turnDamage +=dmg;
-  })
-  totalDamage += turnDamage;
+/**
+ * 指定のダメージを与えたときの平均打点
+ * @param {number} deckNum デッキ枚数
+ * @param {number} cxNum CX枚数
+ * @param {Array.<number>} flipArray ダメージ配列(ex. [2,2,2])
+ * @param {number} loopNum ループ回数
+ */
+function averageDamage (deckNum, cxNum, flipArray, loopNum=10000) {
+  let totalDamage = 0;
+  for (let i=0; i<loopNum; i++){
+    let turnDamage = 0;
+    let deck = ws.createDeck(deckNum,cxNum);
+    // 与えたい打点[2点,2点,2点]
+    const DMG_ARRAY = flipArray;
+    DMG_ARRAY.forEach( dmg => {
+      const res = ws.cancelOrNot(deck,dmg);
+      deck = res.deck;
+      if(!res.cancel) turnDamage +=dmg;
+    })
+    totalDamage += turnDamage;
+  }
+  // 平均打点
+  console.log(totalDamage/loopNum);
+  return totalDamage/loopNum;
 }
-// 平均打点
-console.log(totalDamage/LOOP);
