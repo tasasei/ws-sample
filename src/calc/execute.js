9988b1ce92a02c8499a77c7604ce8dbd5d765603
tasasei <29848455+tasasei@users.noreply.github.com>
2020-11-22 20:47:34 +0900

    create averageConcentration

--- src/calc/execute.js
+++ src/calc/execute.js
@@ -1,26 +1,26 @@
 'use strict';
 
 const ws = require('./wsdeck.js');
-const LOOP = 10000;
 
-// Param
-// Deck Num, CX Num, Flip Num, LOOP Num
-
-// 4枚集中を1回打った時のHit数（率）が知りたい
-let total = 0;
-for(let i=1; i<LOOP; i++){
-  // デッキの内容を設定(CXを含む山札枚数,CX)
-  let deck = ws.createDeck(24,6);
-  // めくる(さっき作ったデッキ,めくる枚数)
-  deck = ws.concentration(deck,4);
-  total += deck.hit;
-};
-// 平均Hit率
-console.log(total/LOOP);
-
-
-// Param
-// Deck Num, CX Num, Flip Num, LOOP Num
+/**
+ * X枚集中を1回打った時のHit数（率）
+ * @param {number} deckNum デッキ枚数
+ * @param {number} cxNum CX枚数
+ * @param {number} flipNumber めくる枚数
+ * @param {number} loopNum ループ回数
+ */
+function averageConcentration (deckNum, cxNum, flipNum, loopNum=10000) {
+  let total = 0;
+  for(let i=1; i<loopNum; i++){
+    // デッキの内容を設定(CXを含む山札枚数,CX)
+    let deck = ws.createDeck(deckNum, cxNum);
+    // めくる(さっき作ったデッキ,めくる枚数)
+    deck = ws.concentration(deck, flipNum);
+    total += deck.hit;
+  };
+  // 平均Hit率
+  return total/loopNum;
+}
 
 /**
  * 指定のダメージを与えたときの平均打点
