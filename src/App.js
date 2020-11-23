5bf2002eee1eefa8f0cc918f69d17385c9d46b66
tasasei <29848455+tasasei@users.noreply.github.com>
2020-11-23 10:56:25 +0900

    move Concentrate

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import React, { useState } from 'react';
 import { averageConcentration, averageDamage } from './calc/execute';
+import './Concentrate'
 import './App.css';
+import Concentrate from './Concentrate';
 
 function App() {
-  const [deckNum, setDeckNum] = useState(50)
-  const [cxNum, setCxNum] = useState(8)
-  const [flipNum, setFlipNum] = useState(4)
-  const [resConcentrate, setResConcentrate] = useState(-1)
-
   return (
     <div className="App">
       <body className="App-body">
-        <div>
-          デッキ枚数
-          <input
-            value={deckNum}
-            onChange={e => setDeckNum(e.target.value)}
-            type="number"
-          />
-        </div>
-        <div>
-          CX枚数
-          <input 
-            value={cxNum}
-            onChange={e => setCxNum(e.target.value)}
-            type="numnber"
-           />
-        </div>
-        <div>
-          めくる枚数
-          <input 
-            value={flipNum}
-            onChange={e => setFlipNum(e.target.value)}
-            type="numnber"
-           />
-        </div>
-        <div>
-          <button
-            onClick={e => setResConcentrate(averageConcentration(deckNum, cxNum, flipNum))}
-          >
-            集中ヒット率計算ボタン
-          </button>
-        </div>
-        <div>
-          集中ヒット率 = {resConcentrate}
-        </div>
+        <Concentrate />
       </body>
     </div>
   );
